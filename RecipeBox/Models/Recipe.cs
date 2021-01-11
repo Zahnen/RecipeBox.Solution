6f1271bc54e3da40b43d01ebb1aef1a5606a2853
Zahnen Garner <zahnen@gmail.com>
2021-01-11 13:02:52 -0800

    add model logic for recipe, ApplicationUser
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Recipe
+  {
+    public Recipe()
+    {
+      this.Tags = new HashSet<RecipeTag>();
+    }
+
+    public int RecipeId { get; set; }
+    public string Ingredients { get; set; }
+    public string Instructions { get; set; }
+    public int Rating { get; set; }
+    public ApplicationUser User { get; set; }
+
+    public ICollection<RecipeTag> Tags { get; }
+  }
+}
---
+++ RecipeBox/Models/Tag.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Tag
+  {
+    public Tag
+    {
+      this.Recipes = new HashSet<Recipe>;
+    }
+
+    public string 
+
+  }
+}
